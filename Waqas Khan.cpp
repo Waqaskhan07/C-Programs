4f870ba9c70e39b57f7532a17e69cbaef9b57506
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-01 19:42:24 +0500

    Add files via upload

---
+++ Waqas Khan.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name ;//declared string
+    name="Muhammad Waqas\n";//write name
+    string id;//declared I'd
+    id="bsf2205783\n";//declared string I'd 
+    cout<<name;//printing name 
+    cout <<id;//printing I'd 
+    int a,b;//assign value of integer variabl
+    a=50;//write value of a 
+    b=10;//write value of b
+    cout<<"\na+b="<<(a+b)<<'endi';//printing the sum value 
+    cout<<"\na-b="<<(a-b)<<'endi';//printing subtract value 
+    cout<<"\na*b="<<(a*b)<<'endi';//printing multiply value 
+    cout<<"\na/b="<<(a/b)<<'endi';//printing the division value 
+    cout<<"\na%b="<<(a%b)<<'endi';//printing modulus value 
+    return 0;
+}
