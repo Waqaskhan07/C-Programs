482f793df95b7ebdbdc52482cfa5d9b0f80d6f83
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-01 22:23:45 +0500

    Add files via upload

---
+++ WK 1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=4;
+    int y=5;     
+cout<<(x<y);
+
+   cout<<(x>y);
+
+cout<<(x<=y);
+
+   cout<<(x>=y);
+
+cout<<(x>2 && y<5);
+
+   cout<<(x<4 || y>3);
+
+cout<<!(x>2 && y<6);
+
+   cout<<!(x<5 && y>6);
+
+    return 0;
+    
+}
