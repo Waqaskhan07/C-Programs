b75387f0d888a269a6fef670e50f3eeaead17205
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-03 10:14:46 +0500

    Add files via upload

---
+++  M Waqas.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=20;
+    int y =10;
+ if(x>y)
+  {
+      cout<<"x is greater than y";
+  }
+ else if(x<y)
+  {
+      cout<<"x is less than y";
+  }
+ else
+  {
+      cout<<"Niether greater nor less";
+  }
+     return 0;
+}
