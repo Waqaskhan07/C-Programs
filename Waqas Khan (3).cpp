d0fc857e40680a72aa1889a0ac44443841aef0f6
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-03 10:09:31 +0500

    Add files via upload

---
+++ Waqas Khan (3).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int time=20;
+ if(time<=18)
+  {
+      cout<<"Good Morning";
+  }
+ else if(time>=18)
+  {
+      cout<<"Good Evening";
+  }
+ else
+  {
+      cout<<"Good night";
+  }
+     return 0;
+}
