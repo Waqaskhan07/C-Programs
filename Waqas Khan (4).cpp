2f5d89f225f2532daf10e3c9182cf35b8103bc05
Waqas Khan <124122507+Waqaskhan07@users.noreply.github.com>
2023-02-05 22:43:20 +0500

    Add files via upload

---
+++ Waqas Khan (4).cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+ if(marks>=90)
+  {
+     cout<<"Grade A"<<endl;
+    cin>>marks;
+  }
+   
+ else if(marks>=80)
+
+  {
+    cout<<"Grade B"<<endl;
+    cin>>marks;
+  }
+
+ else if(marks>=70)
+
+
+  {
+    cout<<"Grade C"<<endl; 
+    cin>>marks;
+  }
+
+ else if(marks>=60)
+
+
+  {
+    cout<<"Grade D"<<endl;
+    cin>>marks;
+  }
+
+ else if(marks>=50)
+
+  { 
+   cout<<"Grade E"<<endl;
+   cin>>marks;
+  }
+ else 
+  {
+   cout<<"Fail";
+  }
+     return 0;
+}
